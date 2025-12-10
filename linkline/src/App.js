db28709e55146326fb21cb3936823f17ce10ee1a
amritha-00 <amrithavemuri@gmail.com>
2025-12-10 19:38:52 +0800

    ok

--- linkline/src/App.js
+++ linkline/src/App.js
@@ -4,7 +4,6 @@ import Content from "./Content";
 import Footer from "./Footer";
 import Cart from "./Cart";
 import Login from "./Login";
-import Navbar from "./Navbar";
 import Signup from "./Signup";
 import Admin from "./Admin";
 import "./App.css";
@@ -12,9 +11,7 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 export default function App() {
   return (
     <div>
-      <Header />
       <BrowserRouter>
-        <Navbar />
         <hr></hr>
         <Routes>
           <Route index element={<Content />} />
@@ -25,8 +22,6 @@ export default function App() {
           <Route path="admin" element={<Admin />} />
         </Routes>
       </BrowserRouter>
-
-      <Footer />
     </div>
   );
 }
